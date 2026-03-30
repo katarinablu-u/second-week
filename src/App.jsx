b254cb9414627ddd81b9f3fb529effd82c95e9a8
katarinablu-u <dodorim55@skuniv.ac.kr>
2026-03-30 20:47:32 +0900

    수절

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>want go home22</h1>
+          <h1>want go home3</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
